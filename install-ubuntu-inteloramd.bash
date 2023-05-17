1bd7a0684846a2ec90bc8273abede07d7d0f4b6d
Pink210 <133900344+Pink210@users.noreply.github.com>
2023-05-17 21:06:12 +0330

    Update install-ubuntu-inteloramd.bash

--- install-ubuntu-inteloramd.bash
+++ install-ubuntu-inteloramd.bash
@@ -30,51 +30,35 @@ fi
 # Remove vpnserver from systemd
 update-rc.d vpnserver remove > /dev/null 2>&1
 
-# Create working directory
-mkdir -p /tmp/softether-autoinstall
-cd /tmp/softether-autoinstall
-
 # Perform apt update & install necessary software
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y && sudo apt install net-tools && sudo apt install ncat
-
-# Install build-essential and checkinstall
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' build-essential|grep "install ok installed")
-echo  "Checking for build-essential: $PKG_OK"
-if [ "" == "$PKG_OK" ]; then
-  echo "build-essential not installed. Installing now."
-  sudo apt install -y build-essential
-fi
-
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' checkinstall|grep "install ok installed")
-echo "Checking for checkinstall: $PKG_OK"
-if [ "" == "$PKG_OK" ]; then
-  echo "checkinstall not installed. Installing now."
-  sudo apt install -y checkinstall
-fi
-
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' build-essential|grep "install ok installed")
-echo  "Checking for build-essential: $PKG_OK"
-if [ "" == "$PKG_OK" ]; then
-  echo "build-essential is still not installed. Possible problem with apt? Exiting."
-  exit 1
-fi
+sleep 5
 
 #install dependency
 sudo apt install gcc binutils gzip libreadline-dev libssl-dev libncurses5-dev libncursesw5-dev libpthread-stubs0-dev
-
+sleep 5
 # Download SoftEther | Version 4.41 | Build 9787
-printf "\nDownloading release: ${RED}4.34 RTM${NC} | Build ${RED}9745${NC}\n\n"
+printf "\nDownloading release: ${RED}4.41 RTM${NC} | Build ${RED}9787${NC}\n\n"
 wget https://www.softether-download.com/files/softether/v4.41-9787-rtm-2023.03.14-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.41-9787-rtm-2023.03.14-linux-x64-64bit.tar.gz
+sleep 2
 tar xvf softether-vpnserver-v4.41-9787-rtm-2023.03.14-linux-x64-64bit.tar.gz
+sleep 3
 cd vpnserver
+sleep 2
 apt install make -y
+sleep 2
 make
+sleep 2
 cd ..
+sleep 2
 sudo mv vpnserver /opt/softether
+sleep 2
 sudo /opt/softether/vpnserver start
+sleep 5
 sudo /opt/softether/vpnserver stop
+sleep 5
 #Openig port
-printf "\Openig port"
+printf "\n${RED}Openig port${NC}\n\n"
 sudo ufw allow 22
 sudo ufw allow 53
 sudo ufw allow 2280
@@ -89,7 +73,8 @@ sudo ufw allow 1701
 sudo ufw allow 500
 sudo ufw allow 8280
 sudo ufw allow 500,4500,8280,53/udp
-
+sleep 5
 echo 1 > /proc/sys/net/ipv4/ip_forward
+sleep 3
 cat /proc/sys/net/ipv4/ip_forward
 esac
