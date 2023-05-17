c69ce4814c5be9171dc345e860d502b59d276819
Pink210 <133900344+Pink210@users.noreply.github.com>
2023-05-18 00:05:50 +0330

    Update install-ubuntu-inteloramd.bash

--- install-ubuntu-inteloramd.bash
+++ install-ubuntu-inteloramd.bash
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-# colors
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-# Execute as sudo
 (( EUID != 0 )) && exec sudo -- "$0" "$@"
 clear
 # User confirmation
@@ -28,49 +24,22 @@ fi
 # Remove vpnserver from systemd
 systemctl disable vpnserver > /dev/null 2>&1
 
+# Start from here
 # Perform apt update & install necessary software
-sudo apt-get update -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y
+sudo apt-get update -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y 
 sleep 2
 
-
-# Install install net-tools
-printf "%sinstall net-tools%s" "$RED" "$NC"
-PKG_OK=$(command -v net-tools)
-echo  "Checking for net-tools: $PKG_OK"
-if [ "" == "$PKG_OK" ]; then
-  echo "net-tools not installed. Installing now."
-  sudo apt-get install -y net-tools
-fi
+# Install some usfull tools
+sudo apt-get install -y certbot && sudo apt-get install -y ncat && sudo apt-get install -y net-tools
 sleep 2
-
-# Install install ncat
-printf "%sinstall ncat%s" "$RED" "$NC"
-PKG_OK=$(command -v ncat)
-echo  "Checking for ncat: $PKG_OK"
-if [ "" == "$PKG_OK" ]; then
-  echo "ncat not installed. Installing now."
-  sudo apt-get install -y ncat
-fi
-sleep 2
-
-# Install install certbot
-printf "%sinstall certbot%s" "$RED" "$NC"
-PKG_OK=$(command -v certbot)
-echo  "Checking for certbot: $PKG_OK"
-if [ "" == "$PKG_OK" ]; then
-  echo "certbot not installed. Installing now."
-  sudo apt-get install -y certbot
-fi
-sleep 2
-
-#install dependency
+# Install dependency
 sudo apt install -y gcc binutils gzip libreadline-dev libssl-dev libncurses5-dev libncursesw5-dev libpthread-stubs0-dev || exit
 sleep 2
+
 # Download SoftEther | Version 4.41 | Build 9787
-printf "%sDownloading release: 4.41 RTM | Build 9787%s" "$RED" "$NC"
 wget https://www.softether-download.com/files/softether/v4.41-9787-rtm-2023.03.14-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.41-9787-rtm-2023.03.14-linux-x64-64bit.tar.gz || exit
 sleep 2
-tar xvf softether-vpnserver-v4.41-9787-rtm-2023.03.14-linux-x64-64bit.tar.gz -C -|| exit
+tar xvf softether-vpnserver-v4.41-9787-rtm-2023.03.14-linux-x64-64bit.tar.gz || exit
 sleep 2
 cd vpnserver || exit
 sleep 2
@@ -86,7 +55,6 @@ sudo /opt/softether/vpnserver start || exit
 sleep 5
 sudo /opt/softether/vpnserver stop || exit
 sleep 5
-printf "%sCreate the service file%s" "$RED" "$NC"
 # Create the service file with the desired content
 sudo tee /etc/systemd/system/softether-vpnserver.service > /dev/null << 'EOF'
 [Unit]
@@ -104,25 +72,20 @@ WantedBy=multi-user.target
 EOF
 
 # Reload the systemd daemon to recognize the new service
-printf "%sdaemon to recognize the new service%s" "$RED" "$NC"
 sudo systemctl daemon-reload || exit
 sleep 2
 # Enable the service to start on boot
-printf "%sEnable the service to start on boot%s" "$RED" "$NC"
 sudo systemctl enable softether-vpnserver.service || exit
 sleep 3
 # Start the service
-printf "%sStart the service%s" "$RED" "$NC"
 sudo systemctl start softether-vpnserver.service || exit
 sleep 5
 # enable IPv4 forwadring 
-printf "%senable IPv4 forwadring%s" "$RED" "$NC"
 echo 1 > /proc/sys/net/ipv4/ip_forward || exit
 sleep 2
 cat /proc/sys/net/ipv4/ip_forward || exit
-#Openig port
 
-printf "%sOpenig port%s" "$RED" "$NC"
+# Openig port
 sudo ufw allow 22
 sudo ufw allow 53
 sudo ufw allow 2280
